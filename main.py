4a2439cb7a01e487e1cf4386993b6536347165a3
wwff1 <73788620+wwff1@users.noreply.github.com>
2023-09-30 23:04:04 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
 from routers import user, table, booking
 
+
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(table.router)
 app.include_router(booking.router)
-
-
-@app.get("/")
-async def get_all():
-    return "О кислород"
